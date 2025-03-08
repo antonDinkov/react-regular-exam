621e427a66cdd8039f416b915d38e472dd19253a
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-03-08 23:05:55 +0200

    Guest page basic structure

--- client/src/components/Guest.jsx
+++ client/src/components/Guest.jsx
@@ -1,18 +1,47 @@
-import Styles from "./Guest.module.css"
+import { useRef } from "react";
+import styles from "./Guest.module.css";
+import { Link } from "react-router";
 
 function Guest() {
+    const searchValue = useRef();
+    const handleSearch = () => {
+        console.log(searchValue.current.value)
+        searchValue.current.value = '';
+    };
+
     return (
-        <div className={Styles.container}>
-            <h1>This is Guest page</h1>
+        <div className={styles.container}>
             <nav>
-                <h1>let see</h1>
+                <h1>At</h1>
                 <ul>
-                    <li><a href="#">At</a></li>
-                    <li><a href="#">At</a></li>
+                    <li><Link to='#'><i className={`fa-solid fa-arrow-right-to-bracket ${styles.i}`}></i><span>Sign in</span></Link></li>
+                    <li><Link to='#'><i className={`fa-regular fa-id-badge ${styles.i}`}></i><span>Create account</span></Link></li>
+                    <li><Link to="/react-regular-exam"><i class={`fa-solid fa-person-through-window ${styles.i}`}></i><span>Leave</span></Link></li>
                 </ul>
             </nav>
+            <main>
+                <section id="search">
+                    <h3>What do you need?</h3>
+                    <input type="search" ref={searchValue} name="search" id="search" placeholder="search" />
+                    <button onClick={handleSearch}>🔍</button>
+                </section>
+                <section id="posts">
+                    <div id="meta">
+                        <img src="#" alt="Profile image" />
+                        <h4>Author</h4>
+                        <p>Date</p>
+                    </div>
+                    <p>Text content</p>
+                    <img src="" alt="Img or Video" />
+                    <div id="feedback">
+                        <p><i className="fa-regular fa-comment"></i><span>5</span></p>
+                        <p><i className="fa-regular fa-heart"></i><span>12</span></p>
+                        <p><i className="fa-solid fa-magnifying-glass"></i><span>57</span></p>
+                    </div>
+                </section>
+            </main>
         </div>
-        
+
     )
 };
 
