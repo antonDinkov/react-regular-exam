f2dcc194bb15b32b2ad9ab2dfcb4b34584e33045
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-03-09 22:16:06 +0200

    guest posts load

--- client/src/components/Guest.jsx
+++ client/src/components/Guest.jsx
@@ -1,13 +1,38 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import styles from "./Guest.module.css";
 import { Link } from "react-router";
+import { db } from "../firebase";
+import { collection, getDocs } from "../firebase";
+import { log } from "mathjs";
 
 function Guest() {
     const searchValue = useRef();
+    const [posts, setPosts] = useState([]);
+    const isDone = useRef(false)
+
     const handleSearch = () => {
         console.log(searchValue.current.value)
         searchValue.current.value = '';
     };
+
+    useEffect(() => {
+        if (isDone.current) return;
+        isDone.current = true;
+        const fetchPost = async () => {
+            try {
+                const postsCollection = collection(db, "posts");
+                const querySnapShot = await getDocs(postsCollection);
+                const postsArray = querySnapShot.docs.map((doc) => doc.data());
+
+                console.log(postsArray);
+                setPosts(postsArray)
+
+            } catch (error) {
+                console.error("Грешка при взимане на постовете:", error);
+            }
+        }
+        fetchPost();
+    }, []);
 
     return (
         <div className={styles.container}>
@@ -42,11 +67,28 @@ function Guest() {
                             <p><i className="fa-solid fa-magnifying-glass"></i><span>57</span></p>
                         </div>
                     </div>
-
+                    {posts.map((pos, index) => {
+                        const postDate = pos.meta.date.toDate()
+                        return (
+                            <div id="post" className={styles.post} key={index}>
+                                <div id="meta" className={styles.meta}>
+                                    <img src={pos.meta.img} alt="Profile image" />
+                                    <h4>{pos.meta.author}</h4>
+                                    <p>{postDate.toLocaleDateString()}</p>
+                                </div>
+                                <p>{pos.content}</p>
+                                <img src={pos.img} alt="Img or Video" />
+                                <div id="feedback" className={styles.feedback}>
+                                    <p><i className="fa-regular fa-comment"></i><span>{pos.feedback.comments}</span></p>
+                                    <p><i className="fa-regular fa-heart"></i><span>{pos.feedback.likes}</span></p>
+                                    <p><i className="fa-solid fa-magnifying-glass"></i><span>{pos.feedback.views}</span></p>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </section>
             </main>
         </div>
-
     )
 };
 
