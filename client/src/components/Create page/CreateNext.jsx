3d28ea5ab69a4dcab5e42e0e61b4ec09f5be6708
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-03-22 14:38:08 +0200

    createNext page basic structure

--- client/src/components/Create page/CreateNext.jsx
+++ client/src/components/Create page/CreateNext.jsx
@@ -1,7 +1,37 @@
+import { Link } from "react-router";
+import styles from './CreateNext.module.css'
+
 function CreateNext() {
     return (
         <form action="">
-            <h1>Hello everybody!</h1>
+            <div className={styles.wrapperMajor}>
+                <header className={styles.header}>
+                    <Link className={styles.x} to='/react-regular-exam'><i className="fa-solid fa-xmark"></i></Link>
+                    <h1 className={styles.logo}>At</h1>
+                </header>
+                <main className={styles.main}>
+                    <div className={styles.wrapper}>
+                        <div className={styles.checkBoxWrapper}>
+                            <div>
+
+                            </div>
+                            <div>
+
+                            </div>
+                            <div>
+
+                            </div>
+                        </div>
+
+                        <p>
+
+                        </p>
+                    </div>
+                    <div className={styles.button}>
+                        <input type="submit" value="Next" />
+                    </div>
+                </main>
+            </div>
         </form>
     )
 };
