96e527513f43d56ae224a6a9555d5b31d4ce34a0
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-03-23 16:13:22 +0200

    register.js finished

--- client/src/components/CreateAccount page/register.js
+++ client/src/components/CreateAccount page/register.js
@@ -1,10 +1,20 @@
-import { createUserWithEmailAndPassword } from "../../firebase";
+import { createUserWithEmailAndPassword, db, doc, setDoc } from "../../firebase";
 
 const registerUser = async (contextData) => {
     try {
-        const {name, email, } = contextData
-        const userdata = await createUserWithEmailAndPassword(auth, EmailAuthCredential, password)
-    } catch (err) {
+        const { name, email, password, birthday, checkbox } = contextData;
+        const { month, day, year } = birthday;
+        const { getMore, connectWith, peronalizedAds } = checkbox;
+        const userdata = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(userdata);
         
+        const user = userdata.user;
+        console.log(user);
+        
+        await setDoc(doc(db, "users", user.uid), {...userdata, createdAt: new Date()});
+    } catch (err) {
+        console.error("Error registering user:", err.message);
     }
 }
+
+export default registerUser;
