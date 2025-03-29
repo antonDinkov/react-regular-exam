3c05829dc55490216f3c191ca374e278d0990180
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-03-29 21:13:04 +0200

    logout functionality

--- client/src/components/Social media/Navbar.jsx
+++ client/src/components/Social media/Navbar.jsx
@@ -1,24 +1,31 @@
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import styles from './Navbar.module.css'
 import { useContext, useEffect, useState } from 'react';
 import { FormContext } from '../../../context/UserContext';
-import { getUser } from '../HTTP/localeStorageApi';
+import { delUserData, getUser } from '../HTTP/localeStorageApi';
+import { auth, signOut } from '../../firebase';
 
 function Navbar() {
     const {formData, updateForm} = useContext(FormContext);
     const [currentUser, setCurrentUser] = useState('Loading...');
+    const navigate = useNavigate();
+    const handleLogout = async (e) => {
+        e.preventDefault();
+        try {
+            await signOut(auth);
+            delUserData();
+            navigate('/react-regular-exam')
+        } catch (error) {
+            console.error("Logout error:", error);
+        }
+    }
 
     useEffect(() => {
         const user = JSON.parse(getUser());
-        console.log('Tochno predi loga');
-        console.log(user);
-        
-        console.log(user.name);
-        
         if (user && user.name) {
             setCurrentUser(user.name);
         } else {
-            setCurrentUser('Guest'); // Ако няма потребител, да не е "Error load..."
+            setCurrentUser('Guest');
         }
     }, []);
     return (
@@ -26,12 +33,12 @@ function Navbar() {
             <h1>At</h1>
             <ul>
                 <li><Link to='/react-regular-exam/welcome'><i className={`fa-solid fa-house-flag ${styles.i}`}></i><span>Welcome</span></Link></li>
-                <li><Link to='/react-regular-exam/create'><i className={`fa-solid fa-bell ${styles.i}`}></i><span>Notification</span></Link></li>
-                <li><Link to='/react-regular-exam/create'><i className={`fa-solid fa-envelope ${styles.i}`}></i><span>Messages</span></Link></li>
-                <li><Link to='/react-regular-exam/create'><i className={`fa-solid fa-user ${styles.i}`}></i><span>Profile</span></Link></li>
-                <li><Link to='/react-regular-exam/create'><i className={`fa-solid fa-suitcase ${styles.i}`}></i><span>Jobs</span></Link></li>
-                <li><Link to="/react-regular-exam"><i className={`fa-solid fa-circle-xmark ${styles.i}`}></i><span>Logout</span></Link></li>
-                <li><Link to="#"><span>Post</span></Link></li>
+                <li><Link to='/react-regular-exam/notification'><i className={`fa-solid fa-bell ${styles.i}`}></i><span>Notification</span></Link></li>
+                <li><Link to='/react-regular-exam/messages'><i className={`fa-solid fa-envelope ${styles.i}`}></i><span>Messages</span></Link></li>
+                <li><Link to='/react-regular-exam/profile'><i className={`fa-solid fa-user ${styles.i}`}></i><span>Profile</span></Link></li>
+                <li><Link to='/react-regular-exam/jobs'><i className={`fa-solid fa-suitcase ${styles.i}`}></i><span>Jobs</span></Link></li>
+                <li><Link onClick={handleLogout} to="#"><i className={`fa-solid fa-circle-xmark ${styles.i}`}></i><span>Logout</span></Link></li>
+                <li><Link to="/react-regular-exam/create"><span>Post</span></Link></li>
 
             </ul>
             <img src="https://png.pngtree.com/png-clipart/20210915/ourmid/pngtree-user-avatar-placeholder-png-image_3918418.jpg" alt="Guest" />
