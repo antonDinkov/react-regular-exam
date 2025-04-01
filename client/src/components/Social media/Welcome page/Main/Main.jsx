80aeeb377848e5e6babf6dd6a2f41148fa684cf4
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-04-01 16:03:21 +0300

    responsive replies counter

--- client/src/components/Social media/Welcome page/Main/Main.jsx
+++ client/src/components/Social media/Welcome page/Main/Main.jsx
@@ -1,17 +1,21 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useOutletContext } from "react-router";
 import styles from "./Main.module.css";
 import Search from "./Search";
 import useMainFunctionality from "./mainFunctionality";
 import { useInfiniteScroll } from "./infinityScrollHook";
 import useDetailsClick from "./detailsClickHook";
+import { getUser } from "../../../HTTP/localeStorageApi";
+import { v4 as uuidv4 } from 'uuid';
+import { getPostById, postComment } from "../../../HTTP/registerAndLogin";
 
 function Main() {
     const searchValue = useRef();
     const { filteredPosts, loadMorePosts, loading, handleSearch } = useMainFunctionality();
     const { mainRef } = useOutletContext();
     const { handleDetailsClick } = useDetailsClick();
-
+    const[commentsNumber, setCommentNumber] = useState('')
+    
     const handleSearchClick = () => {
         if (searchValue.current) {
             handleSearch(searchValue.current.value);
@@ -33,6 +37,29 @@ function Main() {
         theComment.style.display = "none";
     }
 
+    const handleReply = async (e) => {
+        const sourceElement = e.target.closest('#closestID');
+        const postId = sourceElement.getAttribute("data-key");
+        const currentUserInfo = JSON.parse(getUser());
+
+        const data = {
+            id: uuidv4(),
+            author: currentUserInfo.name,
+            text: sourceElement.querySelector('textarea').value,
+            date: new Date().toLocaleString()
+        }
+
+        sourceElement.querySelector('textarea').value = '';
+        if (!data.text) return alert("Your reply is empty!");
+        await postComment(postId, data);
+        const theComment = document.getElementById(postId);
+        theComment.style.display = "none";
+        const num = await getPostById(postId);
+        if(num){
+            const exactNumber = num.feedback.comments;
+            setCommentNumber(exactNumber);
+        }
+    }
 
     return (
         <>
@@ -53,23 +80,19 @@ function Main() {
                                 </div>
                             </div>
                             <div className={styles.feedback}>
-                                <p onClick={(e) => makeVisible(e)}><i className="fa-regular fa-comment"></i><span>{post.feedback.comments}</span></p>
+                                <p onClick={(e) => makeVisible(e)}><i className="fa-regular fa-comment"></i><span>{commentsNumber ? commentsNumber : post.feedback.comments}</span></p>
                                 <p><i className="fa-regular fa-heart"></i><span>{post.feedback.likes}</span></p>
                                 <p><i className="fa-solid fa-magnifying-glass"></i><span>{post.feedback.views}</span></p>
                             </div>
+
                             <div data-key={post.id} id={post.id} style={{ display: "none" }} className={styles.modalOverlay}>
                                 <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
                                     <textarea
                                         className={styles.commentBox}
                                         placeholder="Post your reply"
-                                        onChange={(e) => setMessage(e.target.value)}
                                     />
                                     <div className={styles.modalButtons}>
-                                        <button className={styles.sendBtn} onClick={() => {
-                                            console.log("Изпратено:", message);
-                                            setIsOpen(false);
-                                            setMessage("");
-                                        }}>
+                                        <button className={styles.sendBtn} onClick={(e) => handleReply(e)}>
                                             Reply
                                         </button>
                                         <button className={styles.closeBtn} onClick={(e) => makeInvisible(e)}>
